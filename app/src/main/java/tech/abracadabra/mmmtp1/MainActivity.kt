15fc9a3e63b3410516ac5a03f0980d9d72294234
yaoon <yaoemmanuel33@gmail.com>
2023-01-11 09:50:21 +0000

    Init

---
+++ app/src/main/java/tech/abracadabra/mmmtp1/MainActivity.kt
@@ -0,0 +1,11 @@
+package tech.abracadabra.mmmtp1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
