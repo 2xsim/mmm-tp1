aea994edca0b26b13c1ca0298812851d58e0762b
Mike Prince <simedaprince@gmail.com>
2023-01-20 23:34:39 +0000

    Operations 2/3

--- app/src/main/java/tech/abracadabra/mmmtp1/MainActivity.kt
+++ app/src/main/java/tech/abracadabra/mmmtp1/MainActivity.kt
@@ -1,11 +1,142 @@
 package tech.abracadabra.mmmtp1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var canAddOperation = false
+    private var canCalculate = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun numberAction(view: View) {
+        if (view is Button) {
+            output.append(view.text)
+            canAddOperation = true
+        }
+    }
+
+    fun operationAction(view: View) {
+        if (view is Button) {
+            if (canAddOperation && !canCalculate) {
+                output.append(view.text)
+                canAddOperation = false
+                canCalculate = true
+            } else if (canCalculate) {
+                output.text = calculateResults()
+            }
+        }
+    }
+
+    fun allClearAction(view: View) {
+        output.text = ""
+    }
+
+    fun backSpaceAction(view: View) {
+        val length = output.length()
+        if (length > 0)
+            output.text = output.text.subSequence(0, length - 1)
+    }
+
+    fun equalsAction(view: View) {
+        output.text = calculateResults()
+    }
+
+    private fun calculateResults(): String {
+        val digitsOperators = digitsOperators()
+        if (digitsOperators.isEmpty()) return ""
+
+        val timesDivision = timesDivisionCalculate(digitsOperators)
+        if (timesDivision.isEmpty()) return ""
+
+        val result = addSubtractCalculate(timesDivision)
+        return result.toString()
+    }
+
+    private fun addSubtractCalculate(passedList: MutableList<Any>): Int {
+        var result = passedList[0] as Float
+
+        for (i in passedList.indices) {
+            if (passedList[i] is Char && i != passedList.lastIndex) {
+                val operator = passedList[i]
+                val nextDigit = passedList[i + 1] as Float
+                if (operator == '+') {
+                    result += nextDigit
+                    println("add")
+                }
+                if (operator == '−') {
+                    result -= nextDigit
+                    println("sous")
+                }
+            }
+        }
+
+        return result.toInt()
+    }
+
+    private fun timesDivisionCalculate(passedList: MutableList<Any>): MutableList<Any> {
+        var list = passedList
+        while (list.contains('×') || list.contains('÷')) {
+            list = calcTimesDiv(list)
+        }
+        return list
+    }
+
+    private fun calcTimesDiv(passedList: MutableList<Any>): MutableList<Any> {
+        val newList = mutableListOf<Any>()
+        var restartIndex = passedList.size
+
+        for (i in passedList.indices) {
+            if (passedList[i] is Char && i != passedList.lastIndex && i < restartIndex) {
+                val operator = passedList[i]
+                val prevDigit = passedList[i - 1] as Float
+                val nextDigit = passedList[i + 1] as Float
+                when (operator) {
+                    '×' -> {
+                        newList.add(prevDigit * nextDigit)
+                        restartIndex = i + 1
+                    }
+                    '÷' -> {
+                        newList.add(prevDigit / nextDigit)
+                        restartIndex = i + 1
+                    }
+                    else -> {
+                        newList.add(prevDigit)
+                        newList.add(operator)
+                    }
+                }
+            }
+
+            if (i > restartIndex)
+                newList.add(passedList[i])
+        }
+
+        return newList
+    }
+
+    private fun digitsOperators(): MutableList<Any> {
+        val list = mutableListOf<Any>()
+        var currentDigit = ""
+        for (character in output.text) {
+            if (character.isDigit() || character == '.')
+                currentDigit += character
+            else {
+                list.add(currentDigit.toFloat())
+                currentDigit = ""
+                list.add(character)
+            }
+        }
+
+        if (currentDigit != "")
+            list.add(currentDigit.toFloat())
+
+        return list
     }
 }
